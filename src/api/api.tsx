20c70be4a51d7237ab68df57c857295a51e2835a
Mauricio Holanda <mauricio_holanda@atlantico.com.br>
2024-12-05 14:31:45 -0300

    feat: add url backend

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${process.env.BACKEND_URL}/api`,
+  baseURL: `http://44.201.188.105:1337/api`,
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",

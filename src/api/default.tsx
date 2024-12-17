e94c0e1d07e7bd5555954a90ce2c9f90083cd80c
julioatlantico <julio_goncalves@atlantico.com.br>
2024-12-16 21:47:03 -0300

    Update default.tsx

--- src/api/default.tsx
+++ src/api/default.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiDefault = axios.create({
-  baseURL: "http://34.226.249.133:1337",
+  baseURL: "http://3.88.210.55:1337",
   headers: {
     "Content-Type": "application/json",
     "Access-Control-Allow-Origin": "*",
